098df89c5bd382d285919d8f1851b24fc3a5c209
Jerin <>
2023-07-02 17:18:49 +0530

    script optimization

--- public/script.js
+++ public/script.js
@@ -7,15 +7,16 @@ function submit() {
             "url": document.getElementById("url").value
         }),
         success: function (data) {
-            renderResult(data.url, "p");
+            renderResult(data.url, "p", "result");
         }
     });
 }
 
-function renderResult(data, tagName) {
-    $(tagName).remove();
+function renderResult(data, tagName, id) {
+    $('#' + id).remove();
     textNode = document.createTextNode(data);
     result = document.createElement(tagName);
+    result.setAttribute("id", id);
     result.appendChild(textNode);
     document.getElementById("result-area").appendChild(result);
 }
